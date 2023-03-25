e92669ea63b0f0d6f878444760fa7659024ad928
Antonija Mrkonjic <ja.samanti>
2023-03-25 02:23:05 +0100

    added some screens

---
+++ components/SearchFilter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text,StyleSheet} from 'react-native';
+
+export function SearchFilter({data,input,setInput}){
+    return(
+        <View>
+            <Text>SearchFilter</Text>
+        </View>
+    )
+}
